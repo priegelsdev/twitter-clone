7256126e4ab5236d34ad98278bdda934fcc2b713
priegelsdev <priegelsdev@gmail.com>
2022-11-17 14:15:46 +0100

    create get feed function

--- data.js
+++ data.js
@@ -1,4 +1,10 @@
-export const tweetData = {"name": "random obj",
-  "id": "1234",
-  "color": "what"
-} 
+export const tweetData = [{
+    "name": "random obj",
+    "id": "1234",
+    "color": "what"
+  },
+  { 
+    "name": "random obj2",
+    "id":"5678",
+    "color": "no"    
+  }]
--- index.js
+++ index.js
@@ -2,11 +2,39 @@ import {tweetData} from "/data.js";
 
 const tweetInputText = document.querySelector('.tweet-input-text');
 const tweetBtn = document.querySelector('.tweet-btn');
+const feed = document.querySelector('.feed');
 
-tweetBtn.addEventListener('click', tweetText);
+tweetBtn.addEventListener('click', getFeedHTML);
 
-function tweetText() {
-  console.log(tweetInputText.value)
-  
+
+function getFeedHTML() {
+  for (let tweet of tweetData) {
+    feed.innerHTML += `
+      <div class="tweet">
+        <div class="tweet-inner">
+          <img src="URL OF PROFILE PIC" class="profile-pic">
+          <div>
+            <p class="handle">${tweet.name}</p>
+            <p class="tweet-text">${tweet.color}TWEET TEXT</p>
+            <div class="tweet-details">
+              <span class="tweet-detail">
+                NUMBER OF REPLIES
+              </span> 
+              <span class="tweet-detail">
+                ${tweet.id}
+              </span> 
+              <span class="tweet-detail">
+                NUMBER OF RETWEETS
+              </span>      
+            </div>
+          </div>
+        </div>
+      </div>`
+  }
 }
+
+
+
+
+
 
